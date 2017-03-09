5236d6d19ae64420b6d29c90f964a92386d4704a
ilya101010 <ilya101010@gmail.com>
2017-03-09 10:07:17 +0300

    syng

---
+++ src/idt/irq.c
@@ -0,0 +1,84 @@
+#include <kernel/io.h>
+#include <kernel/ints.h>
+
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
+static void irq_remap(void)
+{
+	outb(0x20, 0x11);
+	outb(0xA0, 0x11);
+	outb(0x21, 0x20);
+	outb(0xA1, 0x28);
+	outb(0x21, 0x04);
+	outb(0xA1, 0x02);
+	outb(0x21, 0x01);
+	outb(0xA1, 0x01);
+	outb(0x21, 0x0);
+	outb(0xA1, 0x0);
+}
+
+void irq_install()
+{
+	irq_remap();
+	idt_set_gate(32,(unsigned)irq0,0x08,0x8E);
+	idt_set_gate(33,(unsigned)irq1,0x08,0x8E);
+	idt_set_gate(34,(unsigned)irq2,0x08,0x8E);
+	idt_set_gate(35,(unsigned)irq3,0x08,0x8E);
+	idt_set_gate(36,(unsigned)irq4,0x08,0x8E);
+	idt_set_gate(37,(unsigned)irq5,0x08,0x8E);
+	idt_set_gate(38,(unsigned)irq6,0x08,0x8E);
+	idt_set_gate(39,(unsigned)irq7,0x08,0x8E);
+	idt_set_gate(40,(unsigned)irq8,0x08,0x8E);
+	idt_set_gate(41,(unsigned)irq9,0x08,0x8E);
+	idt_set_gate(42,(unsigned)irq10,0x08,0x8E);
+	idt_set_gate(43,(unsigned)irq11,0x08,0x8E);
+	idt_set_gate(44,(unsigned)irq12,0x08,0x8E);
+	idt_set_gate(45,(unsigned)irq13,0x08,0x8E);
+	idt_set_gate(46,(unsigned)irq14,0x08,0x8E);
+	idt_set_gate(47,(unsigned)irq15,0x08,0x8E);
+}
+
+void *irq_routines[16] =
+{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0
+};
+
+void irq_install_handler(int irq, void (*handler)(struct regs*))
+{
+	irq_routines[irq] = handler;
+}
+
+void irq_handler(struct regs *r)
+{
+	void (*handler)(struct regs*);
+
+	if (r->int_no >= INTS_MAX_IRQ)
+	{
+		outb(0xA0, 0x20); // wrong int_no => EOI
+	}
+
+	handler = irq_routines[r->int_no - INTS_MAX_ISR];
+	if (handler)
+	{
+		handler(r);
+	}
+
+	// EOI
+	outb(0x20, 0x20);
+}
