0e4b5e07d1ac01c83f7559f942a6ee8f7eccfa6d
ilya101010 <ilya101010@gmail.com>
2017-05-27 00:02:44 +0300

    vmptrld not working. What is the difference bw VMXON region & VMCS?

--- include/kernel/vmx.h
+++ include/kernel/vmx.h
@@ -3,12 +3,12 @@
 
 #include <stdint.h>
 
-// vmx_init.asm todo: get rid of it
+// vmx_init.asm todo: get rid of itvoid vmwrite(int vmcs_id, int value);
 extern char vmx_check();
 
 // vmx_enable.c
 int vmx_init();
 void vmx_crinit();
-void vmx_on();
-
+void vmx_vmxon();
+void vmwrite(uint64_t vmcs_id, uint64_t value);
 #endif
--- src/vmx/vmx_enable.c
+++ src/vmx/vmx_enable.c
@@ -1,21 +1,36 @@
 #include <kernel/vmx.h>
 #include <kernel/msr.h>
 #include <kernel/printf.h>
-#define VMCS 0xffff800000010000
+
+// TODO: stop being stupid
+#define VMCS_L 0xffff800000010000
+#define VMCS_P 0x410000
 
 int size;
 
-void vmx_on(void* paddr)
+void vmx_vmxon(void* paddr)
 {
 	asm("vmxon %0"
 		:"=m"(paddr));
 }
 
+void vmx_vmptrld(unsigned long vmcs)
+{
+unsigned char err;
+
+	__asm__ volatile ("vmptrld %1; setna %0"
+		: "=r"(err)
+		: "m"(vmcs)
+		: "memory", "cc");
+	return err ? -1 : 0;
+}
+
+
 void vmx_crinit()
 {
 	// I HATE INLINE AT&T ASM!
 	// I need it though.
-	// todo: why multiline-inline-assembly needs '\n' ?
+	// todo: why does multiline-inline-assembly need '\n' ?
 	asm("movq %%cr4, %%rax\n"
 		"bts $13, %%rax\n"
 		"movq %%rax, %%cr4\n"
@@ -35,16 +50,18 @@ int vmx_init()
 	vmx_crinit();
 	uint64_t bmsr=0;
 	msr_get(0x480,&bmsr,4+(char*)(&bmsr));
-	printf("VMX revision: 0x%08x\n", bmsr & 0xffffffff);
+	printf("VMX revision: 0x%08x\n", bmsr & 0x8fffffff);
 	size = (bmsr >> 32) & 0x1fff;
 	printf("VMCS size: 0x%d\n",size);
-	if(size == 0) size = 1024;
-	int* rev = 0xffff800000010000;
-	*rev = bmsr & 0xffffffff;
-	uint64_t* addr = 0xffff800000010000;
-	*addr = 0x410000;
-	vmx_on(0x410000);
+	// vmxon region:
+	int* rev = VMCS_L;
+	*rev = bmsr & 0x8fffffff;
+	vmx_vmxon(VMCS_P);
 	printf("vmxon succesful\n");
+	// vmcs:
+	rev += 0x1000;
+	*rev = bmsr & 0x8fffffff;
+	vmx_vmptrld(VMCS_P+0x1000);
 	return 0;
 }
 
