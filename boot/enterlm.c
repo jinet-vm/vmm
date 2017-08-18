3c3e22ed977ba12127c44c296d3b8063a2dc5b39
ilya101010 <ilya101010@gmail.com>
2017-08-18 13:41:13 +0300

    working framebuffer

--- boot/enterlm.c
+++ boot/enterlm.c
@@ -7,6 +7,10 @@ void enterlm(void* mb2_info_tags)
 	uint32_t frst = mb2_info_tags;
 	uint32_t size = *(uint32_t*)mb2_info_tags;
 	mb2_info_tags += 8;
+	uint16_t *t = 0xb8000;
+	// for(int i = 'a'; i<='z'; i++)
+	// 	*t++ = 0x0400+i;
+	// for(;;);
 	while(mb2_info_tags < frst+size)
 	{
 		struct multiboot_tag* tag = mb2_info_tags;
@@ -15,17 +19,17 @@ void enterlm(void* mb2_info_tags)
 			case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
 			{
 				struct multiboot_tag_framebuffer_common* fb = tag;
-				uint64_t* addr = &(fb->framebuffer_addr);
-				uint32_t* a = (uint32_t*)addr;
-				*a++;
-				uint32_t _a = a;
+				uint64_t addr = fb->framebuffer_addr;
+				uint32_t* a = (uint32_t*)&addr;
+				//*a++;
+				uint32_t _a = *a;
 				uint32_t *x = _a;
-				for(int i = 0; i<10000000; i++)
+				for(int i = 0; i<100000; i++)
 				{
 					*x++ = 0xffffffff;
 				}
-				int* r = 1/0;
-				*r = 1/0;
+				//int* r = 1/0;
+				//*r = 1/0;
 				break;
 			}
 			default :
