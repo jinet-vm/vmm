dfecb41cee5cfeb1aef97b35c88a5d7312e26bff
ilya101010 <ilya101010@gmail.com>
2017-09-01 21:24:39 +0300

    sync

--- src/kernel.c
+++ src/kernel.c
@@ -84,13 +84,8 @@ void kernel_start()
 	idt_flush();
 	mprint("IDT flushed");
 
-	struct multiboot_mmap_entry* mmap = bs.lm_mmap_addr;
-	for(int i = 0; i < bs.tr_mmap_len; i++)
-	{
-		uint64_t start = mmap[i].addr;
-		uint64_t end = mmap[i].addr + mmap[i].len;
-		mprint("mmap 0x%x%x - 0x%x%x", start >> 32, start, end >> 32, end);
-	}
+	
+	
 
 	for(;;);
 
