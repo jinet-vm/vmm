f5f0d070332fe910b1ed4444576afe6e52ab35a7
ilya101010 <ilya101010@gmail.com>
2017-09-02 23:27:19 +0300

    pg_get_paddr working?

--- src/mm/paging.c
+++ src/mm/paging.c
@@ -5,8 +5,7 @@
 MODULE("PAGING");
 
 uint64_t pg_get_paddr(uint64_t vma)
-{;
-	mprint("%llx", vma);
+{
 	uint64_t p[5] = 
 	{	vma & 0xfff,
 		(vma >> 12) & 0x1ff,
@@ -18,9 +17,9 @@ uint64_t pg_get_paddr(uint64_t vma)
 	uint64_t np[5] = {0,0,0,0,0};
 	int i; uint64_t faddr;
 
-	for(int i = 3; i >= 0; i--)
+	for(int i = 2; i >= 1; i--)
 	{
-		for(int j = 0; j < 5; j++)
+		for(int j = 0; j < 5; j++) // forming a new recursive mapping request
 			if(i+j >= 5)
 				np[j] = 0xfff;
 			else
@@ -30,42 +29,21 @@ uint64_t pg_get_paddr(uint64_t vma)
 		a |= np[0] & 0xfff;
 		for(int j = 1; j<5; j++)
 			a |= (np[j] & 0x1ffllu) << (12+9*(j-1));
-		if(a & (1llu << 47))
-		{
-			//mprint("g");
+		if(a & (1llu << 47)) // sign bit
 			a |= 0xffff000000000000llu;
-		}
-		uint64_t *p = a;
-		if(!(*p & 1)) // page not present
+		uint64_t *page = a;
+		if(!(*page & 1)) // page not present
 			return 0;
-		if(i == 1 || (*p && (1 << 7))) // not a table
+		if(i == 1 || (*page & (1llu << 7))) // not a table - either page table entry or page bit set
 		{
-			faddr = a;
-			faddr &= ~0xfff;
+			faddr = *page;
+			faddr &= ~0xfffllu;
 			faddr &= ~(1llu << 63); // XD bit
 			faddr |= p[0];
-			for(int j = 0; j < i; j++)
+			for(int j = 1; j < i; j++)
 				faddr |= (p[j] & 0x1ffllu) << (12+9*(j-1));
+			break;
 		}
-		mprint("%llx: %x %x %x %x %x", a, np[4], np[3], np[2], np[1], np[0]);
-		mprint(" => 0x%016llx", *p);
 	}
-
-	// for(i = 0; i >= 0; i--)
-	// {
-	// 	//uint64_t p_a = 0xfff000000000000llu | (0x1ffllu << 39); //~((~0llu) >> (16 + 12 + 9*(i+1)));
-	// 	uint64_t p_a = 0xfff000000000000llu;
-	// 	for(int j = 0; j )
-	// 	p_a |= (vma >> (12+9*i));
-	// 	mprint("%llx", p_a);
-	// 	uint64_t p = *(uint64_t*)p_a;
-	// 	if(i == 0 || (p & (1 << 7))) // page size
-	// 	{
-	// 		faddr = p & ~((1 << (13 + 9*i)) - 1);
-	// 		faddr += vma & ((1 << (13 + 9*i)) - 1);
-	// 		break;
-	// 	}
-	// }
-
 	return faddr;
 }
