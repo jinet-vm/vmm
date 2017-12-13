6d9e8f5068b9bcc248d9b6b6c77f0b9190d49652
truephelle <truephelle@yandex.ru>
2017-12-14 00:21:50 +0300

    Cleanup some files from boot

--- boot/mount.sh
+++ boot/mount.sh
@@ -1,23 +1,54 @@
 #!/bin/bash
 
-DISK=disk.img
-echo "root required"
-LOOPRAW=`losetup -f`
-losetup $LOOPRAW $DISK
-TMP=`kpartx -av $DISK`
-TMP2=${TMP/add map /}
-LOOP=${TMP2%%p1 *}
-LOOPDEV=/dev/${LOOP}
+# Check the script is being run by root 
+if [ "$(id -u)" != "0" ]; then 
+    echo "This script must be run as root\n" 
+    exit 1
+fi
+
+# Get directory of the script
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DISKFILE=disk.img
+DISKPATH="$SCRIPTDIR/$DISKFILE"
+LOOPDRIVE=`losetup --associated $DISKPATH --output NAME --noheadings`
+echo $LOOPDRIVE
+
+if [ -z "$LOOPDRIVE" ]
+then
+    LOOPDRIVE=`losetup -f`
+    losetup $LOOPDRIVE $DISKPATH
+else
+    echo "Already mounted!"
+fi
+
+echo "Waiting for losetup..."
+sleep 1s
+echo "Resuming!"
+
+echo "Connected $DISKFILE to $LOOPDRIVE"
+
+
+KPARTXMSG=`kpartx -av $DISKPATH`
+# cut output string to new devmapping
+MAPNAME=${KPARTXMSG/add map /}
+# add suffix and cut output string from another side
+LOOP=${MAPNAME%%p1 *}
+# puiing it in the standard directory
 LOOPMAP=/dev/mapper/${LOOP}p1
 
-if [ ! -e $LOOPDEV ] ; then
-    echo "Bailing! $LOOPDEV is not valid"
-    exit
-fi
-
-if [ ! -e $LOOPMAP ] ; then
-    echo "Bailing! $LOOPMAP is not valid"
-    exit
-fi
+echo "Waiting for kpartx..."
+sleep 1s
+echo "Mapped loopdrive $LOOPDRIVE to $LOOPMAP partition"
 
 mount ${LOOPMAP} /mnt
+
+
+(cd ..; make clean all)
+
+make clean all
+
+cp boot.elf /mnt/
+cp grub.cfg /mnt/boot/grub/
+cp ../kernel.img /mnt
+umount /mnt
